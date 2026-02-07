c31faa8c828f52620919a57b3d5b08dbbfb638fd
Tomek <tom.dziob@gmail.com>
2026-02-07 20:03:20 +0100

    commit for debug

--- src/track/Waypoint.gd
+++ src/track/Waypoint.gd
@@ -1,9 +1,17 @@
 extends Node2D
 class_name Waypoint
 
+const MAX_RADIUS: float = 400.0
+
 @onready var right_collision: RayCast2D = $RightCollision
 @onready var left_collision: RayCast2D = $LeftCollision
 @onready var label: Label = $Label
+
+var radius: float = MAX_RADIUS:
+	get: return radius
+	
+var radius_factor: float = 0.0:
+	get: return radius_factor	
 
 var number: int = 0:
 	get: return number
@@ -22,8 +30,19 @@ func setup(next_wp: Waypoint, prev_wp: Waypoint, num:int) -> void:
 	next_waypoint = next_wp
 	previous_waypoint = prev_wp
 	number = num
-	label.text = "%d" % num
+	label.text = "%d" % num	
+
+func calc_turn_radius() -> void:
+	var a: float = previous_waypoint.global_position.distance_to(global_position)
+	var b: float = global_position.distance_to(next_waypoint.global_position)
+	var c: float = next_waypoint.global_position.distance_to(previous_waypoint.global_position)
+	var s: float = (a + b + c) / 2.0
+	
+	var area: float  = sqrt(max(s * (s-a) * (s-b) * (s-c), 0.0))
+	
+	if !is_zero_approx(area):
+		radius = (a * b * c) / (4.0 * area)
 	
 func _to_string() -> String:
-	return "%d next: %d prev %d" % [number, next_waypoint.number, previous_waypoint.number]
+	return "%d next: %d prev: %d rad: %.2f factor: %.2f" % [number, next_waypoint.number, previous_waypoint.number, radius, radius_factor]
 	
--- src/ui/TrackSelector.gd
+++ src/ui/TrackSelector.gd
@@ -4,4 +4,4 @@ func _ready() -> void:
 	get_tree().paused = false
 	
 func _on_back_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/GameUI/Main/ModeSelector.tscn")
+	get_tree().change_scene_to_file("res://scenes/GameUI/Menu/ModeSelector.tscn")
